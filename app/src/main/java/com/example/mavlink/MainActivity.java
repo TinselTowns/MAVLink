5f7867736f4a78ccbbe5ad8c805cf26f152d0112
TinselTowns <mariana9608@yandex.ru>
2022-10-18 23:47:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mavlink/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mavlink;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity implements Joystick.JoystickListener{
+
+    Clients mClients;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mClients=new Clients("192.168.1.42",8888);
+        mClients.start();
+
+        Joystick joystick = new Joystick(this);
+        setContentView(joystick);
+
+
+    }
+
+    @Override
+    public void onJoystickMoved(float xPercent, float yPercent,int id)
+    {
+        Log.d("Main Method", "X "+xPercent+" Y "+yPercent);
+    }
+
+
+
+
+
+
+
+
+}
